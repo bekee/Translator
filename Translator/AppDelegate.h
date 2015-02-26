4fec0e3091d21dd771e07158cff97fa8da0bd35e
Mike <wang198904@gmail.com>
2015-02-26 07:29:18 +0200

    Initial Commit

---
+++ Translator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Translator
+//
+//  Created by Passion on 2/26/15.
+//  Copyright (c) 2015 Kite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Translator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Translator
+//
+//  Created by Passion on 2/26/15.
+//  Copyright (c) 2015 Kite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
